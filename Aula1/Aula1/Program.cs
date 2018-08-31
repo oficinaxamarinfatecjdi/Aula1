d057d2eec3482f893010ad244bc417e9bce7f387
Rodrigo de Oliveira Soares <rodrigo.o.soares@hotmail.com>
2018-08-31 19:14:58 -0300

    Código completo

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variável: tipo nome;
+            // Tipos primitivos de variáveis: int, float, double, string 
+            int valor1;
+            int valor2;
+            Console.Write("Escolha o primeiro valor: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escolha o segundo valor: ");
+            valor2 = Convert.ToInt32(Console.ReadLine());
+            int resultado = valor1 + valor2;
+            Console.WriteLine(valor1 + " + " + valor2 + " = " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
